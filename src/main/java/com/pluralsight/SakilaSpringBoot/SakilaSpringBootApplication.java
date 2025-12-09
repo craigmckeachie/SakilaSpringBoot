536777d483017c32ff1d52513f73fbc4d6a34fd9
Craig McKeachie <craigdmckeachie@gmail.com>
2025-12-09 11:08:11 -0500

    add in memory actors

--- src/main/java/com/pluralsight/SakilaSpringBoot/SakilaSpringBootApplication.java
+++ src/main/java/com/pluralsight/SakilaSpringBoot/SakilaSpringBootApplication.java
@@ -1,7 +1,6 @@
 package com.pluralsight.SakilaSpringBoot;
 
-import com.pluralsight.SakilaSpringBoot.data.ActorDaoInMemory;
-import com.pluralsight.SakilaSpringBoot.ui.UserInterface;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
